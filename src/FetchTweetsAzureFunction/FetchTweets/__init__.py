0cd64ee27abc8b750605a1f3fc1e9a6247fb5d98
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-12-20 00:15:43 +0100

    Load twitter credentials

--- src/FetchTweetsAzureFunction/FetchTweets/__init__.py
+++ src/FetchTweetsAzureFunction/FetchTweets/__init__.py
@@ -1,8 +1,16 @@
 import datetime
 import logging
+from collections import namedtuple
+import os
 
 import azure.functions as func
 
+def load_twitter_credentials() -> dict:
+    return {
+        'endpoint': os.getenv('TwitterAPIEndpoint'),
+        'bearer_token': os.getenv('TwitterAPIBearerToken'),
+        'extra_headers_dict': None
+    }
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -11,4 +19,7 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
+    credentials = load_twitter_credentials()
+
+    logging.info(f'Twitter credentials: {credentials}')
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
