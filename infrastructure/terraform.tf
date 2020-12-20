ed7d02c0109d931e5894d420c97f96e9474dd415
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-12-20 10:43:19 +0100

    Switch to Linux based AF

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -71,6 +71,7 @@ resource "azurerm_function_app" "fetchTweetsFA" {
   app_service_plan_id        = azurerm_app_service_plan.fetchTweetsASP.id
   storage_account_name       = azurerm_storage_account.storage.name
   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  os_type                    = "linux"
   version                    = "~3"
 
   app_settings = {
