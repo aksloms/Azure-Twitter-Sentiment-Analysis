53eaa428db79af66d511fe655b00baf6af45bcf3
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-12-20 10:39:03 +0100

    Fetch tweets for each hashtag

--- src/FetchTweetsAzureFunction/FetchTweets/__init__.py
+++ src/FetchTweetsAzureFunction/FetchTweets/__init__.py
@@ -9,7 +9,7 @@ from typing import Union, Dict, List, Sequence
 
 import azure.functions as func
 
-def load_twitter_credentials() -> Dict[str, str]:
+def load_twitter_credentials() -> Dict[str, Union[str, None]]:
     return {
         'endpoint': os.getenv('TwitterAPIEndpoint'),
         'bearer_token': os.getenv('TwitterAPIBearerToken'),
@@ -22,43 +22,47 @@ def get_hashtags() -> List[str]:
 
 def filter_tweets(tweets: Sequence[Dict[str, str]]) -> Sequence[Dict[str, str]]:
     '''Discard unusable tweets'''
-    return (t for t in tweets if t['lang'] == 'pl')
+    return (t for t in tweets if t.get('lang', None) == 'pl')
 
 def main(mytimer: func.TimerRequest, fetchedTweetsQue: func.Out[func.QueueMessage]) -> None:
     time = datetime.utcnow().replace(tzinfo=timezone.utc)
+    
     hashtags = get_hashtags()
-
     credentials = load_twitter_credentials()
-    querry = hashtags[-1] # TODO: Fetch tweets for all hashtags
     start_time = time - timedelta(minutes=5)
     tweet_fields = ['id', 'text', 'created_at', 'lang']
 
-    hashtag = hashtags[-1] # TODO: Fetch tweets for all hashtags
-    retquest_params = gen_request_parameters(
-        hashtag,
-        # start_time = start_time.strftime("%Y-%m-%d %H:%M"),
-        results_per_call = 100,
-        tweet_fields=','.join(tweet_fields),
-        # since_id= # TODO: Use last fetch tweet id in request
-    )
+    for hashtag in hashtags:
+        query = hashtag
 
-    response = collect_results(retquest_params,
-        max_tweets=100,
-        result_stream_args=credentials
-    )
+        logging.info(f'Fetching tweets with query: {query}')
+        retquest_params = gen_request_parameters(
+            query,
+            start_time = start_time.strftime("%Y-%m-%d %H:%M"),
+            tweet_fields=','.join(tweet_fields),
+            # since_id= # TODO: Use last fetch tweet id in request
+        )
 
-    if (response):
-        tweets = response[:-1]
-        response_metadata = response[-1]
-        # TODO: Store 'newest_id'
-        # TODO: Support pagination
+        response = collect_results(retquest_params,
+            max_tweets=100,
+            result_stream_args=credentials
+        )
 
-        messages = []
-        for t in filter_tweets(tweets):
-            t['hashtag'] = hashtag
-            messages.append(dumps(t))
+        if (response):
+            tweets = response[:-1]
+            response_metadata = response[-1]
+            # TODO: Store 'newest_id'
+            # TODO: Support pagination
 
-        logging.info(messages)
-        fetchedTweetsQue.set(messages)
+            logging.info(f'Unfiltered tweets count: {len(tweets)}')
+
+            messages = []
+            for t in filter_tweets(tweets):
+                t['hashtag'] = hashtag
+                messages.append(dumps(t))
+
+            logging.info(f'Filtered tweets count: {len(messages)}')
+            logging.info(messages)
+            fetchedTweetsQue.set(messages)
 
     logging.info('Python timer trigger function ran at %s', time.isoformat())
