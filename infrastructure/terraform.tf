67d71a8fb2333ae0224788f6809be7eb96a09069
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-12-20 10:57:19 +0100

    Adjust app service plan to support linux AF

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -50,6 +50,7 @@ resource "azurerm_app_service_plan" "fetchTweetsASP" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "FunctionApp"
+  reserved            = true
 
   sku {
     tier = "Dynamic"
