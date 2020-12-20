2ed056ce11349b24a795f6e2a7decc165af2660d
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-12-20 10:20:16 +0100

    Save tweets to que

--- src/FetchTweetsAzureFunction/FetchTweets/__init__.py
+++ src/FetchTweetsAzureFunction/FetchTweets/__init__.py
@@ -3,11 +3,13 @@ import logging
 import os
 from datetime import datetime, timedelta, timezone
 from searchtweets import gen_request_parameters, collect_results
-from typing import List
+from json import dumps
+
+from typing import Union, Dict, List, Sequence
 
 import azure.functions as func
 
-def load_twitter_credentials() -> dict:
+def load_twitter_credentials() -> Dict[str, str]:
     return {
         'endpoint': os.getenv('TwitterAPIEndpoint'),
         'bearer_token': os.getenv('TwitterAPIBearerToken'),
@@ -18,9 +20,9 @@ def get_hashtags() -> List[str]:
     # TODO: Get hashtags from shared configuration like storage table or Application Settings
     return ['#kwarantanna', '#vege', '#IgaŚwiatek', '#hot16challenge', '#fitness', '#krolowezycia', '#kryzys', '#ikea', '#łódź', '#halloween', '#kawa', '#radom', '#karmieniepiersia', '#pomidorowa', '#COVID19', '#nvidia', '#poniedziałek', '#biedronka']
 
-def filter_tweets():
-    # TODO: Discard unusable tweets. Maybe filter by language
-    pass
+def filter_tweets(tweets: Sequence[Dict[str, str]]) -> Sequence[Dict[str, str]]:
+    '''Discard unusable tweets'''
+    return (t for t in tweets if t['lang'] == 'pl')
 
 def main(mytimer: func.TimerRequest, fetchedTweetsQue: func.Out[func.QueueMessage]) -> None:
     time = datetime.utcnow().replace(tzinfo=timezone.utc)
@@ -31,8 +33,9 @@ def main(mytimer: func.TimerRequest, fetchedTweetsQue: func.Out[func.QueueMessag
     start_time = time - timedelta(minutes=5)
     tweet_fields = ['id', 'text', 'created_at', 'lang']
 
+    hashtag = hashtags[-1] # TODO: Fetch tweets for all hashtags
     retquest_params = gen_request_parameters(
-        querry,
+        hashtag,
         # start_time = start_time.strftime("%Y-%m-%d %H:%M"),
         results_per_call = 100,
         tweet_fields=','.join(tweet_fields),
@@ -43,12 +46,19 @@ def main(mytimer: func.TimerRequest, fetchedTweetsQue: func.Out[func.QueueMessag
         max_tweets=100,
         result_stream_args=credentials
     )
-    tweets = response[:-1] if response else []
-    response_metadata = response[-1] if response else []
 
-    fetchedTweetsQue.set(["Test message 1", "Test message 2"])
+    if (response):
+        tweets = response[:-1]
+        response_metadata = response[-1]
+        # TODO: Store 'newest_id'
+        # TODO: Support pagination
 
-    for r in response:
-        logging.info(r)
+        messages = []
+        for t in filter_tweets(tweets):
+            t['hashtag'] = hashtag
+            messages.append(dumps(t))
+
+        logging.info(messages)
+        fetchedTweetsQue.set(messages)
 
     logging.info('Python timer trigger function ran at %s', time.isoformat())
