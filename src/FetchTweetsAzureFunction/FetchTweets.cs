01bbb71df712f2fc8f7006dec140cfcb108619c9
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-12-24 21:35:52 +0100

    Put messages into queue

--- src/FetchTweetsAzureFunction/FetchTweets.cs
+++ src/FetchTweetsAzureFunction/FetchTweets.cs
@@ -11,13 +11,17 @@ using FetchTweetsAzureFunction.Extensions;
 using Tweetinvi.Parameters.V2;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text.Json;
 
 namespace FetchTweetsAzureFunction
 {
     public static class FetchTweets
     {
         [FunctionName("FetchTweets")]
-        public static async Task RunAsync([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task RunAsync(
+            [TimerTrigger("0 */5 * * * *")]TimerInfo myTimer,
+            [Queue("tweetsque"), StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
+            ILogger log)
         {
             var config = new ConfigurationBuilder()
                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
@@ -53,8 +57,15 @@ namespace FetchTweetsAzureFunction
                 var tweets = result.Tweets.Where(t => t.Lang == "pl");
                 log.LogInformation($"Hashtag: {hasthtag}");
                 log.LogInformation(string.Join("\n", tweets.Select(t => t.Text).Take(3)));
-            }
 
+                foreach (var tweet in tweets)
+                {
+                    var id = tweet.Id;
+                    var text = tweet.Text;
+                    var message = JsonSerializer.Serialize(new {hasthtag, text, id});
+                    msg.Add(message);
+                }
+            }
 
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
