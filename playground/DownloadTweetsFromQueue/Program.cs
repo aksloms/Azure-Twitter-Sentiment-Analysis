52dc87fe258aaeb4d6c6981c3789a1eb05287c68
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2021-01-03 16:24:35 +0100

    Create project for saving messages from the queue

---
+++ playground/DownloadTweetsFromQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace DownloadTweetsFromQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddYamlFile("appsettings.yml")
+                .Build();
+
+            Console.WriteLine(config.GetConnectionString("MainStorage"));
+        }
+    }
+}
