8dc7fe7e4b11e52ad1ef33778ea75c32a6663649
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2021-01-03 17:20:42 +0100

    Serialize tweets

--- playground/DownloadTweetsFromQueue/Program.cs
+++ playground/DownloadTweetsFromQueue/Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 using Azure.Storage.Queues; // Namespace for Queue storage types
 using Azure.Storage.Queues.Models; // Namespace for PeekedMessage
@@ -27,11 +28,19 @@ namespace DownloadTweetsFromQueue
             // Convert.FromBase64String()
             var message = ReadQueueMessage(peekedMassges.Value[0].Body);
             Console.WriteLine(message);
-            var tweet = JsonSerializer.Deserialize<Tweet>(message, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            Console.WriteLine(tweet.Text);
+            // var tweet = JsonSerializer.Deserialize<Tweet>(message, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            // Console.WriteLine(tweet.Text);
+
+            var tweets = peekedMassges.Value
+                .Select(m => ReadQueueMessage(m.Body))
+                .ToArray();
+
+            Console.WriteLine(JsonSerializer.Serialize(tweets, new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase }));
         }
 
-        public static string ReadQueueMessage(BinaryData messageData)
-            => Encoding.UTF8.GetString(Convert.FromBase64String(messageData.ToString()));
+        public static Tweet ReadQueueMessage(BinaryData messageData)
+            => JsonSerializer.Deserialize<Tweet>(
+                Encoding.UTF8.GetString(Convert.FromBase64String(messageData.ToString())),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
     }
 }
