9b0cabb0b1df59be57680737e266e6a53bc6764a
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2021-01-05 18:26:44 +0100

    Deserialize tweet and prepare model for table storage

--- src/LabelTweetsAzureFunction/LabelTweets.cs
+++ src/LabelTweetsAzureFunction/LabelTweets.cs
@@ -1,16 +1,43 @@
 using System;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using LabelTweetsAzureFunction.Models;
 
 namespace LabelTweetsAzureFunction
 {
     public static class LabelTweets
     {
         [FunctionName("LabelTweets")]
-        public static void Run([QueueTrigger("tweetsque", Connection = "QueueStorageConnection")] string myQueueItem, ILogger log)
+        public static void Run([QueueTrigger("tweetsque", Connection = "QueueStorageConnection")] string queueMessage, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            var serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var tweet = JsonSerializer.Deserialize<Tweet>(queueMessage);
+
+            tweet.CleanedText = CleanTweetText(tweet);
+
+            log.LogInformation($"C# Queue trigger function processed: {queueMessage}");
+        }
+
+        public static string CleanTweetText(Tweet tweet)
+        {
+            var markersToRemove = tweet.Hashtags.AsEnumerable<ITextMarker>()
+                .Concat(tweet.Urls)
+                .OrderBy(m => m.Start);
+
+            var builder = new StringBuilder(tweet.Text.Length);
+            var currentPostion = 0;
+            foreach (var marker in markersToRemove)
+            {
+                builder.Append(tweet.Text[currentPostion..marker.Start]);
+                currentPostion = marker.End;
+            }
+
+            return builder.ToString();
         }
     }
 }
---
+++ src/LabelTweetsAzureFunction/models/Tweet.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace LabelTweetsAzureFunction.Models
+{
+    public class Tweet : ITableRecord
+    {
+        public string RowKey => Id;
+        public string PartitionKey => SearchHashtag;
+        public string SearchHashtag { get; set; }
+        public string Id { get; set; }
+        public string AuthorId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public IList<HashtagMarker> Hashtags { get; set; }
+        public IList<UrlMarker> Urls { get; set; }
+        public string Text { get; set; }
+
+        #region labeling data
+        public string CleanedText { get; set; }
+        public double CognitiveServicesSentiment { get; set; }
+        #endregion
+    }
+
+    public class HashtagMarker : ITextMarker
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public string Tag { get; set; }
+    }
+
+    public class UrlMarker : ITextMarker
+    {
+        public string DisplayUrl { get; set; }
+        public int End { get; set; }
+        public string ExpandedUrl { get; set; }
+        public int Start { get; set; }
+        public string Url { get; set; }
+        public string UnwoundUrl { get; set; }
+    }
+}
