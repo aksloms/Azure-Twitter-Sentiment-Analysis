9eba17f0b94bda50d8ba37deddeb3be2c920258c
Radek C <=>
2021-01-13 23:39:51 +0100

    Code cleanup. Variable renamed.

--- backend_app/SampleTweets.py
+++ backend_app/SampleTweets.py
@@ -13,15 +13,15 @@ class SampleTweets(Resource):
         end_date = request.args.get("end_date")
         count = int(request.args.get("count"))
 
-        entities = self.client.query_entities(
+        tweet_rows = self.client.query_entities(
             filter="CreatedAt ge datetime'" + start_date + "' and CreatedAt lt datetime'" + end_date + "'",
             select=[u"SearchHashtag", u"Text", u"CognitiveServicesSentimentScore"])
 
         response_content = []
         i = 0
-        for e in entities:
-            del e["_metadata"]
-            response_content.append(e)
+        for tweet_row in tweet_rows:
+            del tweet_row["_metadata"]
+            response_content.append(tweet_row)
             if i == count:
                 break
 
