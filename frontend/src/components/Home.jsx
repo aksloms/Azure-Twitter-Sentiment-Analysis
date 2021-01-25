48d2dc38861f997aaa3145f4275620662f5ea46e
DamianSmu <smugorzewski.ds@gmail.com>
2021-01-25 18:07:12 +0100

    Home page change

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,114 +1,105 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import { Grid, Typography } from '@material-ui/core';
+import {Grid, Typography} from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
-import MainPlot from '../recources/main_plot.jpg'
-import FirstPlot from '../recources/first_plot.png'
-import SecPlot from '../recources/sec_plot.jpeg'
+import FirstPlot from '../resources/first_plot.png'
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 
 const useStyles = makeStyles((theme) => ({
-    title: {
-        textAlign: "center",
-        marginLeft: "10%",
-        marginRight: "10%"
-    },
-    plot_container: {
-        height: "75%",
-        position: "fixed",
-        bottom: 0
-    },
-    remove_space: {
-        position: "relative",
-        padding: 0,
-        margin: 0
-    },
-    menuButton: {
-        width: "80%",
-        height: "15%",
-        position: "absolute",
-        bottom: 0
-    },
     button: {
+        marginTop: 10,
+        width: 200
+    },
+    section: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    card: {
+        maxWidth: 1320,
+        marginTop: 15
+
+    },
+    media: {
+        width: 1290,
+        height: 500,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    h5: {
+        color: '#FFFFFF',
+        marginBottom: 30,
+        marginTop: 50,
+    },
+    h2: {
+        paddingTop: 80,
+        color: '#FFFFFF',
+    },
+    content: {
         textAlign: "center",
-        width: "100%",
-        height: "100%"
+        width: "50%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: 30,
+        marginBottom: 30
     }
 }));
 
-var backgroundStyleMainPlot = {
-    backgroundImage: `url(${MainPlot})`,
-    width: "100%",
-    height: "100%",
-}
-
-var backgroundStyleFirstPlot = {
-    backgroundImage: `url(${FirstPlot})`,
-    width: "100%",
-    height: "100%",
-}
-
-var backgroundStyleSecPlot = {
-    backgroundImage: `url(${SecPlot})`,
-    width: "100%",
-    height: "100%",
-}
-
-
 export default function Home() {
     const classes = useStyles();
-
     return (
-        <React.Fragment>
+        <div className={classes.section}>
+            <Card className={classes.card}>
 
-            <Typography className={classes.title}>
-                <h1>
-                    Analiza sentymentu oparta o wskazany aspekt
-                </h1>
-                <h3>
-                    Aplikacja pozwalająca na zwizualizowanie zmieniających się nastrojów komentarzy na Twitter w zależności od ustalonyego hashtag oraz słów wpływających na zmianę sentymentu treści wpisu
-                </h3>
-            </Typography>
+                <CardMedia
+                    className={classes.media}
+                    image={FirstPlot}
+                >
+                    <Typography color="inherit" align="center" variant="h2" marked="center" className={classes.h2}>
+                        Analiza sentymentu tweetów
+                    </Typography>
+                    <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+                        Aplikacja wizualizująca sentyment tweetów w języku polskim
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        size={"large"}
+                        className={classes.button}
+                        component="a"
+                        href="/plot/sentiment"
+                    >
+                        Analiza
+                    </Button>
+                </CardMedia>
+                <CardContent className={classes.content}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Cel projektu
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Aplikacja pozwalająca na zwizualizowanie zmieniających się nastrojów komentarzy na Twitter w
+                        zależności od ustalonego hashtag oraz słów wpływających na zmianę sentymentu treści wpisu.
+                    </Typography>
 
-            <Grid container className={classes.plot_container}>
-
-                <Grid item xs={6}>
-                    <Container style={backgroundStyleMainPlot}>
-                    </Container>
-                </Grid>
-
-                <Grid item xs={6}>
-                    <Grid container className={classes.plot_container}>
-
-                        <Grid item xs={12}>
-                            <Container className={classes.remove_space} style={backgroundStyleFirstPlot}>
-                                <div className={classes.menuButton}>
-
-                                    <Button className={classes.button} variant="contained" color="primary" href="/plot/sentiment" >
-                                        Analiza sentymentu
-                                    </Button>
-                                </div>
-                            </Container>
-                        </Grid>
-
-                        <Grid item xs={12}>
-                            <Container className={classes.remove_space} style={backgroundStyleSecPlot}>
-                                <div className={classes.menuButton}>
-
-                                    <Button className={classes.button} variant="contained" color="secondary" href="/plot/aspect" >
-                                        Analiza aspektu
-                                    </Button>
-                                </div>
-                            </Container>
-                        </Grid>
-
-                    </Grid>
-                </Grid>
-            </Grid>
-
-        </React.Fragment>
-
+                    <Typography gutterBottom variant="h5" component="h2" style={{paddingTop: "40px"}}>
+                        Repozytorium projektu
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        size={"large"}
+                        component="a"
+                        href="https://github.com/aksloms/Azure-Twitter-Sentiment-Analysis"
+                        startIcon={<GitHubIcon/>}
+                    >
+                        Github
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
     );
 }
